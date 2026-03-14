2b37e0b34fe2cf5113e361e15d9842960d33f5c1
hareem284 <sehrishandaleeb06@gmail.com>
2026-03-14 14:28:21 +0500

    commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#creating an empty dictionary
+empty_dict={}
+my_dict={1: 'jack', 2: 'lauren'}
+string_dict={'name': 'hareem', 'age': 11}
+mixed_keys={'name': 'hareem', 1:[4,3,2,6,8]}
+print(string_dict['name'])
+print(mixed_keys[1])
+my_dict[3]='hareem'
+print(my_dict)
+my_dict.pop(2)
+print(my_dict)
+print(string_dict['age'])
